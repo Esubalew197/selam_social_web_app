1aa6c87f20687f2aea8663f9b25bd20951816b2b
Esubalew197 <Esubalewmillion@gmail.com>
2023-11-06 15:28:01 +0000

    update

--- selam_app/selam_app/settings.py
+++ selam_app/selam_app/settings.py
@@ -21,7 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0*n2u-yvipu1=g*#923g30uxyqf%!)62#slwyq%n+#q@@gy)fw'
+#SECRET_KEY = 'django-insecure-0*n2u-yvipu1=g*#923g30uxyqf%!)62#slwyq%n+#q@@gy)fw'
+
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
