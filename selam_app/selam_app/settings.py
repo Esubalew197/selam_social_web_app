c87efeafcf49b9e497becdbc09e5431b149edf78
Esubalew197 <109427994+Esubalew197@users.noreply.github.com>
2023-11-11 01:51:41 +0300

    updating html files

--- selam_app/selam_app/settings.py
+++ selam_app/selam_app/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'wana'
+    'bootstrap4',
+    'wana',
 ]
 
 MIDDLEWARE = [
